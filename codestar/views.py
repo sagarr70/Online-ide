75332499c63dd0b7168e58e55781bb564cbcc870
sagar aggarwal <sagar_aggarwal@outlook.com>
2021-12-19 21:17:02 +0530

    added support for c language

--- codestar/views.py
+++ codestar/views.py
@@ -76,6 +76,44 @@ def runcode(request):
             print(output)
             # res = render(request,'codestar/home.html',{"code":code_part,"input":y,"output":output})
             res = HttpResponse(output)
+
+
+        if (language=='c'):
+
+            with open("one.c",'w') as file:
+                 file.write(code_part)
+                 file.close()
+            temp_path=os.getcwd()
+            print(temp_path)
+            # input_part = input_part.replace("\n","")
+            os.system('type nul > input.txt')
+            with open("input.txt",'w') as file:
+                file.write(input_part)
+                file.close()
+            # subprocess.run(['g++','one.cpp'])
+            # os.system('g++ one.cpp')
+            error=subprocess.run(['gcc','one.c'], capture_output=True,shell=True)
+            success=True
+            if(error.stderr.decode('UTF-8')!=''):
+                result=error.stderr.decode('UTF-8')
+                success=False
+            else:
+                result=subprocess.run(['a.exe','<','input.txt'], capture_output=True,shell=True)
+                result=result.stdout.decode('UTF-8')
+            # os.system('a.exe < input.txt > output.txt')
+            # with open ("output.txt", "r") as myfile:
+                #  result=myfile.read()
+            # if(result2==''):
+                # result=result.stderr.decode('UTF-8')
+            # print(result)
+
+            # os.remove('output.txt')
+            # if(success):
+            #   os.remove('a.exe')
+            
+            # res = render(request,'codestar/home.html',{"code":code_part,"input":input_part,"output":result})
+            res = HttpResponse(result)
+
     return res
 def report(request):
     res =render(request,'codestar/report.html')
