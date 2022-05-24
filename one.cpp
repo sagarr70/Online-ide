6eed664faa05d9cced144e71561405b7da4c4a5a
sagar aggarwal <sagar_aggarwal@outlook.com>
2022-05-24 14:19:07 +0530

    zunaira update

--- one.cpp
+++ one.cpp
@@ -1,8 +1,11 @@
- #include<iostream>
- using namespace std; 
- int main()
- { 
- 
- cout<<"hello JAVA "; 
- return 0; 
+ #include<iostream>
+ using namespace std; 
+ int main()
+ { int n;
+ int m;
+ cin>>m>>n;
+ int s = m+n;
+ 
+ cout<<s; 
+ return 0; 
 }
